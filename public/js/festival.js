443c5e4d2dc1ea491fa158c7bf0f12253bdc83dd
Martin Suckau <martin.suckau@web.de>
2020-01-09 22:49:31 +0100

    restyling...

--- public/js/festival.js
+++ public/js/festival.js
@@ -26,39 +26,6 @@
     target: '#sidebar'
   });
 
-    function initMap() {
-        var location = new google.maps.LatLng(51.38035,11.4315363);
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-            center: location,
-            zoom: 13,
-            panControl: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(mapCanvas, mapOptions);
-
-        var marker = new google.maps.Marker({
-            position: location,
-            map: map
-        });
-
-        var contentString = '<div class="info-window">' +
-            '<a target="_blank" href="https://www.google.com/maps/@51.38035,11.4315363,17z?hl=de">Tarmac Festival</a>' +
-            '</div>';
-
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
-        });
-
-        var styles = [{"featureType":"administrative","elementType":"all","stylers":[{"saturation":"-100"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"saturation":-100},{"lightness":"50"},{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":"-100"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"lightness":"30"}]},{"featureType":"road.local","elementType":"all","stylers":[{"lightness":"40"}]},{"featureType":"transit","elementType":"all","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]},{"featureType":"water","elementType":"labels","stylers":[{"lightness":-25},{"saturation":-100}]}];
-        map.set('styles', styles);
-    }
-    google.maps.event.addDomListener(window, 'load', initMap);
-
   $('#sidebarCollapse').on('click', function () {
       $('#sidebar').toggleClass('active');
       $('#content').toggleClass('active');
