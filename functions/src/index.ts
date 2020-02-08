4bf090366d0fceb20a0616e26bb9cb766373658a
Martin Suckau <martin.suckau@web.de>
2020-02-08 22:18:22 +0100

    fixed stuff

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,32 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const rp = require('request-promise');
+const serviceAccount = require("../key/festival-allstedt-firebase-adminsdk-k9w4f-32e9d571b2.json");
 
-admin.initializeApp();
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://festival-allstedt.firebaseio.com"
+});
 
 exports.checkRecaptcha = functions.https.onRequest((req, res) => {
+    if (!req.body.token) {
+        throw new functions.https.HttpsError('invalid-argument');
+    }
+    if (!req.body.email) {
+        throw new functions.https.HttpsError('invalid-argument');
+    }
+    if (!req.body.message) {
+        throw new functions.https.HttpsError('invalid-argument');
+    }
+    if (!req.body.subject) {
+        throw new functions.https.HttpsError('invalid-argument');
+    }
+
     const token = req.body.token;
+    const email = req.body.email;
+    const message = req.body.message.replace(/<script|<\/script/gi, "");
+    const subject = req.body.subject.replace(/<script|<\/script/gi, "");
+
     console.log("recaptcha response token", token);
     rp({
         uri: 'https://recaptcha.google.com/recaptcha/api/siteverify',
@@ -19,10 +40,10 @@ exports.checkRecaptcha = functions.https.onRequest((req, res) => {
         console.log("recaptcha result", result);
         if (result.success && result.score >= 0.6) {
             admin.firestore().collection('mail').add({
-                to: [req.body.email],
+                to: [email],
                 message: {
-                    html: req.body.message,
-                    subject: req.body.subject
+                    html: message,
+                    subject: subject
                 }
             });
             res.json({result: 'success'});
